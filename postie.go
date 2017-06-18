0cb7588a0247c3fddc746466187c08db8c80402e
James Eagle <jimeagle@gmail.com>
2017-06-18 21:22:46 +0100

    Hello world

---
+++ postie.go
@@ -0,0 +1,13 @@
+package main
+
+import "io"
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world!")
+}
