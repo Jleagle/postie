6bc3a15b9474d84834ee7861aeb48a4eadef2d79
James Eagle <james.eagle@fortifi.it>
2017-06-19 18:49:54 +0100

    Create random string and redirect to it

--- postie.go
+++ postie.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
 
+	"github.com/elgs/gostrgen"
+	"github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 )
 
@@ -17,30 +21,66 @@ func main() {
 
 	e.Renderer = t
 
-	e.GET("/", home)
-	e.GET("/new", new)
-	e.GET("/:id", requests)
-	e.GET("/:id/x", requests)
+	e.GET("/", homeRoute)
+	e.GET("/new", newRoute)
+	e.Any("/:id", requestsRoute)
+	e.GET("/:id/x", requestsRoute)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-func home(c echo.Context) error {
-	return c.Render(http.StatusOK, "home", "James")
+func homeRoute(c echo.Context) error {
+	return c.Render(http.StatusOK, "home", "xx")
 }
 
-func new(c echo.Context) error {
-	return c.String(http.StatusOK, "New")
+func newRoute(c echo.Context) error {
+
+	randomString, err := gostrgen.RandGen(10, gostrgen.Upper|gostrgen.Digit, "", "")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/postie")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	for {
+		fmt.Println("loop")
+		insert, err := db.Query("INSERT INTO urls (url) VALUES (?)", "test")
+		defer insert.Close()
+
+		if err == nil {
+			break
+		} else {
+			fmt.Println("err")
+			if sqlerr, ok := err.(*mysql.MySQLError); ok {
+				fmt.Println("mysql err")
+				if sqlerr.Number == 1062 { // Duplicate entry
+					fmt.Println("1062 err")
+					continue
+				}
+			}
+
+			panic(err.Error())
+		}
+	}
+
+	return c.Redirect(http.StatusTemporaryRedirect, "/"+randomString)
 }
 
-func requests(c echo.Context) error {
+func requestsRoute(c echo.Context) error {
 	return c.String(http.StatusOK, "Requests")
 }
 
+// Template xxx
 type Template struct {
 	templates *template.Template
 }
 
+// Render xxx
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
