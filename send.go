addd8b8986ca4c79e81d186d3fd7a830da275d2c
James Eagle <jimeagle@gmail.com>
2018-08-19 19:13:21 +0100

    Fix build

--- send.go
+++ send.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/andelf/go-curl"
 )
 
 func sendRoute(w http.ResponseWriter, r *http.Request) {
@@ -14,21 +12,4 @@ func postSendRoute(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-
-	easy.Setopt(curl.OPT_URL, "http://google.com")
-
-	// make a callback function
-	fooTest := func(buf []byte, userdata interface{}) bool {
-		println("DEBUG: size=>", len(buf))
-		println("DEBUG: content=>", string(buf))
-		return true
-	}
-
-	easy.Setopt(curl.OPT_WRITEFUNCTION, fooTest)
-
-	if err := easy.Perform(); err != nil {
-		Error(err)
-	}
 }
